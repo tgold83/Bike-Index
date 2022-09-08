2ef39b35aa417b1bbb7ec5357aa350b7a7115b47
Ted Gold <110305659+tgold83@users.noreply.github.com>
2022-09-08 13:08:45 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+// Business Logic
+
+function getWeather(city) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, city);
+    } else {
+      printError(this, response, city);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+// UI Logic
+
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
+function printElements(apiResponse, city) { 
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+  The temperature in Fahrenheit is ${(1.8 * (apiResponse.main.temp-273))+32} degrees.
+  The temperature in Kelvin is ${apiResponse.main.temp} degrees.
+  The timezone is ${apiResponse.timezone}.
+  The tempurature feels like ${apiResponse.main.feels_like} degrees Kelvin.
+  Sunrise will occur at ${apiResponse.sys.sunrise} UTC.
+  Sunset will occur at ${apiResponse.sys.sunset} UTC.`; 
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
